11cf6f82ef02e6db34574e15793fc658a7203770
Markus <markus.niederbrucker@gmx.at>
2013-03-18 11:50:27 +0100

    Update and rename mQUery.js to mQuery.js
    added initial draft of method id

---
+++ mQuery.js
@@ -0,0 +1,19 @@
+(function ($)
+{
+	"use strict";
+
+	$.extend($.fn, {
+		id: function (id)
+		{
+			if (id && typeof("id") == "string")
+			{
+				return this.each(function ()
+				{
+					$(this).attr("id", id);
+				});
+			}
+			else
+				return this[0].attr("id");
+		}
+	}
+})(jQuery);
