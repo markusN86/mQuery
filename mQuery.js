d0a38f421930f55392ec3df2451e08e9c281168c
Mex <markus@niederbrucker.at>
2013-03-20 13:58:53 +0100

    new functions prevCycle, nextCycle, center, centerHorizontal

--- mQuery.js
+++ mQuery.js
@@ -27,6 +27,102 @@
 		src: function (src)
 		{
 			return _attribute.apply(this, ["src", src]);
-		}		
+		},
+		nextCycle: function ()
+		{
+			var $el = this.next();
+
+			if (!$el.length)
+				$el = this.parent().children(":first-child");
+
+			return $el;
+		},
+		prevCycle: function ()
+		{
+			var $el = this.prev();
+
+			if (!$el.length)
+				$el = this.parent().children(":last-child");
+
+			return $el;
+		},
+		center: function(w, h)
+		{
+			var parent = this.parent(),
+				pHeight = h || parent.innerHeight(),
+				pWidth = w || parent.innerWidth(),
+				elHeight = this.outerHeight(),
+				elWidth = this.innerWidth(),
+				ratio = elWidth / elHeight,
+				height = elHeight, width = elWidth, top = 0, left = 0;
+
+			if (ratio > 1)
+			{
+				if (elWidth < pWidth)
+					left = (pWidth - elWidth) / 2;
+				else if (elWidth > pWidth)
+				{
+					width = pWidth;
+					left = 0;
+					height = width / ratio;
+				}
+
+				if (height < pHeight)
+					top = (pHeight - height) / 2;
+				else if (height > pHeight)
+				{
+					height = pHeight;
+					top = 0;
+					width = height * ratio;
+					left = (pWidth - width) / 2;
+				}
+			}
+			else if (ratio < 1)
+			{
+				if (elHeight < pHeight)
+					top = (pHeight - elHeight) / 2;
+				else if (elHeight > pHeight)
+				{
+					height = pHeight;
+					top = 0;
+					width = height * ratio;
+				}
+
+				if (width < pWidth)
+					left = (pWidth - width) / 2;
+				else if (width > pWidth)
+				{
+					width = pWidth;
+					left = 0;
+					height = width / ratio;
+					top = (pHeight - height) / 2;
+				}
+			}
+
+			return this.css({
+				top: top,
+				left: left,
+				width: width,
+				height: height
+			});
+		},
+		centerHorizontal: function ()
+		{
+			var parent = this.parent(),
+				pHeight = parent.height(),
+				height = this.height(),
+				newHeight = height,
+				top;
+
+			if (height > pHeight)
+				newHeight = pHeight;
+
+			top = (pHeight - newHeight) / 2;
+
+			return this.css({
+				top: top,
+				height: newHeight
+			});
+		}
 	});
 })(jQuery);
