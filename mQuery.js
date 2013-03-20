c288447a674e4c72a7f5ec341c25f7114634c0e1
Markus <markus.niederbrucker@gmx.at>
2013-03-20 12:32:59 +0100

    refactoring

--- mQuery.js
+++ mQuery.js
@@ -5,39 +5,27 @@
 	$.extend($.fn, {
 		id: function (id)
 		{
-			if (id && typeof(id) == "string")
-			{
-				return this.each(function ()
-				{
-					$(this).attr("id", id);
-				});
-			}
-			else
-				return this[0].attr("id");
+			return this._attribute("id", id);
 		},
 		title: function (title)
 		{
-			if (title && typeof(id) == "string")
-			{
-				return this.each(function ()
-				{
-					$(this).attr("title", title);
-				});
-			}
-			else
-				return this[0].attr("title");
+			return this._attribute("title", title);
 		},
 		src: function (src)
 		{
-			if (src && typeof(id) == "string")
+			return this._attribute("src", src);
+		},
+		_attribute: function (name, value)
+		{
+			if (value && typeof(value) == "string")
 			{
 				return this.each(function ()
 				{
-					$(this).attr("src", src);
+					$(this).attr(name, value);
 				});
 			}
 			else
-				return this[0].attr("src");
+				return this[0].attr(name);
 		}
 	});
 })(jQuery);
